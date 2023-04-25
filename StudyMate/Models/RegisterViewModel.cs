7be3f858b38fefc4ab5b486fa4c66b159712a8da
Andrea Cassina <cassina99@gmail.com>
2023-04-25 20:59:46 +0200

    Aggiunti register e login con campi custom

---
+++ StudyMate/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyMate.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string DegreeCourse { get; set; }        // codice del corso di studi
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
