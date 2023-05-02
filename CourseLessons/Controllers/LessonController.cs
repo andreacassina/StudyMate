39287373331ff2a94d9fb4b67aefc931da96fdda
Andrea Cassina <cassina99@gmail.com>
2023-05-02 23:18:47 +0200

    - Aggiunto progetto Web API per la richiesta dei corsi e degli orari delle lezioni - Aggiunto servizio per le richieste alle Web API

---
+++ CourseLessons/Controllers/LessonController.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace CourseLessons.Controllers
+{
+    public class LessonController : ControllerBase
+    {
+
+        [HttpGet("GetLessons")]
+        //[HttpGet]
+        public IEnumerable<Lesson> Get(string degreeCourse, string courseName)
+        {
+            //D:\Projects\StudyMate1\CourseLessons\Courses\
+            string filePath = @"Courses\" + degreeCourse + @"\" + courseName + ".csv";
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Lesson>().ToList();
+                return records;
+            }
+
+
+        }
+
+        
+    }
+}
---
+++ StudyMate/Services/IDegreeCourseService.cs
@@ -0,0 +1,8 @@
+﻿namespace StudyMate.Services
+{
+    public interface IDegreeCourseService
+    {
+        bool searchDegreeCourseCourses(string degreeCourse);       //Funzione che cerca se esistono i corsi di uno specifico corso di laurea (do per scontato che se ne esiste uno esistono tutti)
+        void DownloadCourses(string degreeCourse);
+    }
+}
